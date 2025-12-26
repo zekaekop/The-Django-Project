da4510889700337f6590c91892a4912d2d4a0759
Eko <zekaeko.p@gmail.com>
2025-12-26 12:33:45 +0300

    Added other clouds final

--- static/js/clouds.js
+++ static/js/clouds.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const cloudContainers = document.querySelectorAll('body');
     
     const cloud_images = [
-        '/static/img/clouds/cloud1_trans.png',
-        // '/static/img/clouds/cloud2.png',
-        // '/static/img/clouds/cloud3.png',
-        // '/static/img/clouds/cloud4.png',
+        '/static/img/clouds/cloud1.png',
+        '/static/img/clouds/cloud2.png',
+        '/static/img/clouds/cloud3.png',
+        '/static/img/clouds/cloud4.png',
     ];
 
     cloudContainers.forEach(function(container) {
@@ -18,13 +18,13 @@ document.addEventListener('DOMContentLoaded', function() {
         cloud.src = cloud_images[Math.floor(Math.random() * cloud_images.length)];
         cloud.className = "cloud";
 
-        cloud.style.left = Math.random() * -10 + - 10 +  "%";
-        cloud.style.top = Math.random() * 70 + 10 + "%";
-        cloud.style.scale = Math.random() * 0.5 + 0.2;
+        cloud.style.left = (Math.random() * -15 + -20) +  "%";
+        cloud.style.top = (Math.random() * 60 + 10) + "%";
+        cloud.style.scale = (Math.random() * 0.5 + 0.2);
         
         // Randomize animation delay and duration
-        cloud.style.animationDelay = (Math.random() * 10)+ "s";
-        cloud.style.animationDuration = (Math.random() * 20 + 30) + "s";
+        cloud.style.animationDelay = (Math.random() * 25)+ "s";
+        cloud.style.animationDuration = (Math.random() * 30 + 50) + "s";
         
         container.appendChild(cloud);
         }
