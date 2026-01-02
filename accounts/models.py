0642236f6d0563e96be63347032df92cd235d069
Eko <zekaeko.p@gmail.com>
2026-01-02 15:15:43 +0300

    The user mod admin layout idea

--- accounts/models.py
+++ accounts/models.py
@@ -1,15 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-#class User_account(models.Model):
-#    about = models.CharField(max_length=100)
-#    location = models.CharField(max_length=100)
-#    account_age = models.PositiveIntegerField(default=18)
-#
-#    select_gender = (
-#        ('Other', 'Other'),
-#        ('Male', 'Male'),
-#        ('Female', 'Female'),)
-#    gender = models.CharField(max_length=8, choices=select_gender, default="other")
-#
-#    image = models.ImageField(upload_to='profile_pics', null=True, blank=True)
+
+class UserRegularMailAccount(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    about = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    account_age = models.DateTimeField(verbose_name="Date/Time ", auto_now_add=True)
+
+    select_gender = (
+        ('Other', 'Other'),
+        ('Male', 'Male'),
+        ('Female', 'Female'),)
+    
+    gender = models.CharField(max_length=8, choices=select_gender, default="other")
+
+    image = models.ImageField(upload_to='profile_pics', null=True, blank=True)
+
+class UserModerator(models.Model):
+    pass
+
+class UserAdmin(models.Model):
+    pass
