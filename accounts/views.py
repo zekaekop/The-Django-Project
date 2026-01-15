6520034a32f6849a77041784178f8d47bcb2ba48
Eko <zekaeko.p@gmail.com>
2026-01-15 13:17:36 +0300

    Added warnings for exact name and for passwords bellow 8 char

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,7 @@ from post.models import Post
 from django.contrib.auth.models import User
 from django.db.models import Q
 from django.core.paginator import Paginator
+from django.contrib import messages
 
 # ----- Main Account logic -----
 def login_view(request):
@@ -34,20 +35,10 @@ def login_view(request):
 
 def signin_view(request):
     form = RegisterForm(request.POST or None)
-    if form.is_valid():
-        user = form.save()
-        password = form.cleaned_data.get('password')
-        user.set_password(password)
-        user.is_staff = False
-        user.is_superuser = False
-        user.save()
-        new_user = authenticate(username = user.username, password = password)
-        login(request, new_user)
-        return redirect('/')
     
     posts = Post.objects.count()
     accounts = User.objects.count()
-    
+        
     context ={
         'form':form,
         'title':'Login',
@@ -55,6 +46,25 @@ def signin_view(request):
         "total_accounts":accounts,
         "total_posts":posts,
         }
+
+    if form.is_valid():
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+
+        if User.objects.filter(username=username).exists():
+            messages.warning(request, 'Username already exists. Please choose a different one.')
+            return render(request, "account_templates/form.html", context)
+        if len(password) < 8:
+            messages.warning(request, 'Password must be at least 8 characters long.')
+            return render(request, "account_templates/form.html", context)
+        user = form.save()
+        user.set_password(password)
+        user.is_staff = False
+        user.is_superuser = False
+        user.save()
+        new_user = authenticate(username = user.username, password = password)
+        login(request, new_user)
+        return redirect('/')
         
     return render(request, "account_templates/form.html", context)
 
