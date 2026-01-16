aa6f1e4e69ab4ad21e5a8bcf38294e9755783cc7
Eko <zekaeko.p@gmail.com>
2026-01-16 22:22:17 +0300

    added profile pics on headersearch

---
+++ post/templatetags/profile_tag.py
@@ -0,0 +1,23 @@
+from django import template
+from django.contrib.auth.models import User
+from accounts.models import UserProfile  # IMPORTANT: Import your model
+from accounts.views import create_user_profile # IMPORTANT: Import your function
+
+register = template.Library()
+
+@register.inclusion_tag('profile_templates/generated_profile_pic.html')
+def render_profile(user_or_profile=None):
+    context = {}
+    
+    if user_or_profile:
+        if isinstance(user_or_profile, User):
+            # Get profile from user
+            try:
+                context['profile'] = UserProfile.objects.get(user=user_or_profile)
+            except UserProfile.DoesNotExist:
+                context['profile'] = create_user_profile(user_or_profile, user_or_profile)
+        else:
+            # Assume it's already a profile object
+            context['profile'] = user_or_profile
+    
+    return context
