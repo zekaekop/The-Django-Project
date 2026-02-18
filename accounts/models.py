5ca891f4da64006c8a63323c68032670add387e5
Eko <zekaeko.p@gmail.com>
2026-02-18 16:51:53 +0300

    Added heart and broken heart icons and friendship many2many modal

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+AUTH_USER_MODEL = 'accounts.UserProfile'
+
 class UserProfile(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
@@ -18,3 +20,18 @@ class UserProfile(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
+# Handles the model friendship relation system
+# a friendship can be sent and it will be pending
+# friendship invites can be declined or accepted
+
+# Source - https://stackoverflow.com/a/52915586
+# Posted by Hugo Trentesaux
+# Retrieved 2026-02-18, License - CC BY-SA 4.0
+
+class Friend(models.Model):
+    to_user = models.ForeignKey(AUTH_USER_MODEL, models.CASCADE, related_name='friends')
+    from_user = models.ForeignKey(AUTH_USER_MODEL, models.CASCADE, related_name='_unused_friend_relation')
+
+class FriendshipRequest(models.Model):
+    from_user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='friendship_requests_sent')
+    to_user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='friendship_requests_received')
