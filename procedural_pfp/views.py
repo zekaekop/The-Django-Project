9ebcb0b25666fb7133bc3a72b11c2299d59a453e
Eko <zekaeko.p@gmail.com>
2026-02-25 16:08:56 +0300

    fixes signing in

--- procedural_pfp/views.py
+++ procedural_pfp/views.py
@@ -15,17 +15,12 @@ class PfpAssembly():
         result = self.create_user_pfp(request.user)
         return redirect("/profile/" + request.user.username)
 
-    def create_user_pfp(self, user, new_user = None):
+    def create_user_pfp(self, user, new_user=None):
         # generate pfp
         generated_pic_data = self.generate_pfp_values(4, 4)
-        
-        # Try to get existing profile or create new one
         profile, created = UserProfile.objects.get_or_create(user=user)
-        
-        # Update the generated_pic field
         profile.generated_pic = generated_pic_data
         profile.save()
-        # This isnt really required since the profile checks if profile is created before fetching 
 
     # class Pixel: # I removed this since i forgot the model uses JSONField meaning its just way easier to do a dict
     #     def __init__(self, r, g, b, a=255):
